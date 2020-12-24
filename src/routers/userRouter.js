5f7bb1ef14bba6d8753b5fc1925f288b62684ea7
VishalBangera1997 <vishal.x@mphasis.com>
2020-12-24 15:49:42 +0530

    Added try catch statements

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -26,50 +26,58 @@ userRouter.post('/users/login', async (req, res) => {
     }
 });
 
-userRouter.patch('/users/logout',auth,async(req,res)=>{
-    try{
-    req.user.tokens=req.user.tokens.filter((token)=>{
-        return token.token !== req.token;
-    })
-    await req.user.save();
-    res.send();
-    }catch(e){
+userRouter.patch('/users/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token;
+        })
+        await req.user.save();
+        res.send();
+    } catch (e) {
         res.status(401).send();
     }
 });
 
-userRouter.patch('/users/logoutall',auth,async(req,res)=>{
-    try{
-    req.user.tokens=undefined;
-    await req.user.save();
-    res.send();
-    }catch(e){
-      res.status(500).send();
+userRouter.patch('/users/logoutall', auth, async (req, res) => {
+    try {
+        req.user.tokens = undefined;
+        await req.user.save();
+        res.send();
+    } catch (e) {
+        res.status(500).send();
     }
-})
+});
 
 userRouter.patch('/bulb', auth, async (req, res) => {
-    const User = req.user;
-    const operations = Object.values(req.query);
-    // let allowedOperations = [true, false];
-    // let validOperation = operations.every((operation) => allowedOperations.includes(operation));
-    // if (!validOperation) {
-    //     return res.status(400).send('Invalid Operation');
-    // }
-    User.devices.bulb = req.query.status;
-    await User.save();
-    res.set('Content-Type', 'text/plain');
-    res.send({ value: User.devices.bulb });
-    // let host="192.168.4.1";
-    // let url="/data/?value=1";
-    // request(String("GET ") + url + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" +"Connection: close\r\n\r\n");  
+    try {
+        const User = req.user;
+        const operations = Object.values(req.query);
+        // let allowedOperations = [true, false];
+        // let validOperation = operations.every((operation) => allowedOperations.includes(operation));
+        // if (!validOperation) {
+        //     return res.status(400).send('Invalid Operation');
+        // }
+        User.devices.bulb = req.query.status;
+        await User.save();
+        res.set('Content-Type', 'text/plain');
+        res.send({ value: User.devices.bulb });
+        // let host="192.168.4.1";
+        // let url="/data/?value=1";
+        // request(String("GET ") + url + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" +"Connection: close\r\n\r\n");  
+    } catch (e) {
+        res.status(500).send();
+    }
 });
 
 
 
 
-userRouter.get('/bulb',async (req, res) => {
-    const user = await User.findById(req.query.userId);
-    res.set('Content-Type', 'text/plain');// set the headers 
+userRouter.get('/bulb', async (req, res) => {
+    try {
+        const user = await User.findById(req.query.userId);
+        res.set('Content-Type', 'text/plain');// set the headers 
         res.send({ bulbStatus: user.devices.bulb });// if led is on or true, send bulbStatus=true
+    } catch (e) {
+        res.status(500).send();
+    }
 });

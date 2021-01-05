8e256c36feda109e9784a7d9028ae285c985aa75
VishalBangera1997 <vishal.x@mphasis.com>
2021-01-05 13:51:57 +0530

    added build

--- src/db/mongodb.ts
+++ src/db/mongodb.ts
@@ -7,7 +7,8 @@ export const mongoDb = () => {
     mongoose.connect(process.env.MONGO_CONNECTION_STRING!, {
         useNewUrlParser: true,
         useCreateIndex: true,
-        useFindAndModify: false
+        useFindAndModify: false,
+        useUnifiedTopology:true
     });
 
     var db = mongoose.connection;

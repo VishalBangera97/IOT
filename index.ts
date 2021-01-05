8b335390d990418b24d776f3c5deb74ea126d9a7
VishalBangera1997 <vishal.x@mphasis.com>
2021-01-05 14:03:48 +0530

    updated index

--- index.ts
+++ index.ts
@@ -6,17 +6,13 @@ import { config } from 'dotenv';
 config();
 
 let port = process.env.PORT!;
-// if (cluster.isMaster) {
-//     for (let i = 0; i < os.cpus().length; i++) {
-//         cluster.fork();
-//     }
-// } else {
-//     let server = http.createServer(app);
-//     server.listen(port, () => {
-//         console.log('Server is listening at port number ' + port);
-//     });
-// }
-let server = http.createServer(app);
-server.listen(port, () => {
-    console.log('Server is listening at port number ' + port);
-});
+if (cluster.isMaster) {
+    for (let i = 0; i < os.cpus().length; i++) {
+        cluster.fork();
+    }
+} else {
+    let server = http.createServer(app);
+    server.listen(port, () => {
+        console.log('Server is listening at port number ' + port);
+    });
+}
